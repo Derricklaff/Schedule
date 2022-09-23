db42e5c5f6885f40b895b315a7a231827d9f6789
Derrick Lafforthun <derricklaff@gmail.com>
2022-09-22 17:02:31 -0700

    added all planner elements in html

---
+++ planner.js
@@ -0,0 +1,39 @@
+// get current day and time; link to inner HTML
+// header date 
+var today = moment().format('LLL');
+$("#currentDay").text(today);
+
+// Local time
+var localTime = moment().format('HH');
+console.log(localTime)
+
+// save to local storage and save when refreshed
+$(".saveBtn").on("click", function (event) {
+    event.preventDefault();
+
+    var userInfo = $(this).prev().val();
+    console.log(userInfo)
+    localStorage.setItem($(this).prev().attr("id"), userInfo)
+
+});
+
+// time blocks for past, present, and future time
+// change colors based on local time
+$("input").each(function () {
+    var selectedInput = parseInt($(this).attr("id"));
+
+    if (selectedInput > localTime) {
+        $(this).addClass("future")
+
+    } else if (selectedInput == localTime) {
+
+        $(this).addClass("present")
+
+    }
+    else {
+        $(this).addClass("past")
+
+    };
+
+    $("#" + selectedInput).val(localStorage.getItem(selectedInput))
+});
