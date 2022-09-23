64043303a2aed058ca054fc99e24cfc61103dfb9
Derrick Lafforthun <derricklaff@gmail.com>
2022-09-22 17:28:23 -0700

    Created current time const for js

--- planner.js
+++ planner.js
@@ -1,39 +1,7 @@
-// get current day and time; link to inner HTML
-// header date 
-var today = moment().format('LLL');
-$("#currentDay").text(today);
+const currentDay = moment().format('dddd LL')
+// console.log(currentDay)
+let $currentDay = $('#currentDay')
 
-// Local time
-var localTime = moment().format('HH');
-console.log(localTime)
+let currentTime = moment().format('h:mm A')
 
-// save to local storage and save when refreshed
-$(".saveBtn").on("click", function (event) {
-    event.preventDefault();
-
-    var userInfo = $(this).prev().val();
-    console.log(userInfo)
-    localStorage.setItem($(this).prev().attr("id"), userInfo)
-
-});
-
-// time blocks for past, present, and future time
-// change colors based on local time
-$("input").each(function () {
-    var selectedInput = parseInt($(this).attr("id"));
-
-    if (selectedInput > localTime) {
-        $(this).addClass("future")
-
-    } else if (selectedInput == localTime) {
-
-        $(this).addClass("present")
-
-    }
-    else {
-        $(this).addClass("past")
-
-    };
-
-    $("#" + selectedInput).val(localStorage.getItem(selectedInput))
-});
+$currentDay.text(currentDay)
