0c6565d30bfaf7d9dafd5cad240b435d7c8b58cd
Derrick Lafforthun <derricklaff@gmail.com>
2022-09-22 18:14:50 -0700

    finished js to meet parameters

--- planner.js
+++ planner.js
@@ -5,3 +5,31 @@ let $currentDay = $('#currentDay')
 let currentTime = moment().format('h:mm A')
 
 $currentDay.text(currentDay)
+
+$(".saveBtn").on("click", function (event) {
+    event.preventDefault();
+
+    let userInfo = $(this).prev().val();
+    console.log(userInfo)
+    localStorage.setItem($(this).prev().attr("id"), userInfo)
+
+});
+
+$("input").each(function () {
+    let selectedInput = parseInt($(this).attr("id"));
+
+    if (selectedInput > currentTime) {
+        $(this).addClass("future")
+
+    } else if (selectedInput == currentTime) {
+
+        $(this).addClass("present")
+
+    }
+    else {
+        $(this).addClass("past")
+
+    };
+
+    $("#" + selectedInput).val(localStorage.getItem(selectedInput))
+});
