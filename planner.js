0444f599a16b96fbf479226716c19b045a5e3359
Derrick Lafforthun <derricklaff@gmail.com>
2022-09-23 15:33:03 -0700

    finished code now working on readme

--- planner.js
+++ planner.js
@@ -1,119 +1,35 @@
-const currentDay = moment().format('dddd LL')
-// console.log(currentDay)
-let $currentDay = $('#currentDay')
 
-let currentTime = moment().format('h:mm A')
+//Header date
+let $currentDay = moment().format('LL');
+$('#currentDay').text($currentDay);
 
+//Local time
+let $localTime = moment().format('HH');
+console.log($localTime)
 
-let $9amInput = $('#9am');
-let $10amInput = $('#10am');
-let $11amInput = $('#11am');
-let $12pmInput = $('#12pm');
-let $1pmInput = $('#1pm');
-let $2pmInput = $('#2pm');
-let $3pmInput = $('#3pm');
-let $4pmInput = $('#4pm');
-let $5pmInput = $('#5pm');
-
-arrayOf$Times = [$9amInput, $10amInput, $11amInput, $12pmInput, $1pmInput, $2pmInput, $3pmInput, $4pmInput, $5pmInput]
-
-$currentDay.text(currentDay)
-
-for (i = 0; i < 9; i++) {
-    if (moment("3pm", 'h A').isAfter(moment('9 AM', 'h A').add(i, 'h'))) {
-        arrayOf$Times[i].addClass('past');
-    }
-    else if (moment('3pm', 'h A').isSame(moment('9 AM', 'h A').add(i, 'h'))) {
-        arrayOf$Times[i].addClass('present');
-    } else if (moment('3pm', 'h A').isBefore(moment('9 AM', 'h A').add(i, 'h'))) {
-        arrayOf$Times[i].addClass('future');
-    }
-
-}
-
-
+// Save when refreshed
 $('.saveBtn').on('click', function (event) {
     event.preventDefault();
-    let $userInfo = $(this).siblings('input').val();
-    let $localCurrent = localStorage.getItem('entry') || "";
-    if ($userInfo) {
-        localStorage.setItem('entry', $userInfo)
+
+    let $user = $(this).prev().val();
+    localStorage.setItem($(this).prev().attr('id'), $user);
+});
+
+$('input').each(function () {
+    var selectedInput = parseInt($(this).attr("id"));
+
+    if (selectedInput > $localTime) {
+        $(this).addClass("future")
+
+    } else if (selectedInput == $localTime) {
+
+        $(this).addClass("present")
+
     }
+    else {
+        $(this).addClass("past")
 
+    };
 
-
-})
-
-
-$9amInput.val(localStorage.getItem('entry'));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // $(".saveBtn").on("click", function (event) {
-//     event.preventDefault();
-
-//     let userInfo = $(this).prev().val();
-//     console.log(userInfo)
-//     localStorage.setItem($(this).prev().attr("id"), userInfo)
-
-// });
-
-// $("input").each(function () {
-//     let selectedInput = parseInt($(this).attr("id"));
-
-//     if (selectedInput > currentTime) {
-//         $(this).addClass("future")
-
-//     } else if (selectedInput == currentTime) {
-
-//         $(this).addClass("present")
-
-//     }
-//     else {
-//         $(this).addClass("past")
-
-//     };
-
-//     $("#" + selectedInput).val(localStorage.getItem(selectedInput))
-// })
+    $("#" + selectedInput).val(localStorage.getItem(selectedInput))
+});
